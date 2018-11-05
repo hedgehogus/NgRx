6a4403beb209d989dd43603fc570e36f9965a860
hedgehogus <research162@gmail.com>
2018-11-05 23:22:47 +0200

    initial

---
+++ myNgRx/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CarsFormComponent } from './cars-form/cars-form.component';
+import { CarComponent } from './car/car.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarsFormComponent,
+    CarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
