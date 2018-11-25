97e0f67b5f4f0f48a784b22ee264613ed7a50a7a
hedgehogus <research162@gmail.com>
2018-11-26 00:43:29 +0200

    ngrx action

--- myNgRx/src/app/app.component.ts
+++ myNgRx/src/app/app.component.ts
@@ -8,10 +8,7 @@ import { Cars, Car } from './car.model'
 })
 
 export class AppComponent {
-  public cars: Car [] = [
-    new Car('Ford', '12.12.12', 'Focus', false, 1),
-    new Car('Audi', '08.08.12', 'A4', false, 2)
-  ]; 
+  public cars: Car [] = [ ]; 
   
   onAdd(car:Car){
     this.cars.push(car);
